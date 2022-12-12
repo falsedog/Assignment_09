e5938a216ed7af82f2e27b5dfb0c134ab69d58b8
falsedog <117885158+falsedog@users.noreply.github.com>
2022-12-11 21:19:44 -0800

    First upload
    Included txt files so the scripts are a little happier, and to see them

---
+++ IOClasses.py
@@ -0,0 +1,216 @@
+# Title: IO Classes
+# Desc: A Module for IO Classes
+# Change Log: (Who, When, What)
+# Rain Doggerel, 2022-Dec-06, First touch
+# DBiesinger, 2030-Jan-01, Created File
+# DBiesinger, 2030-Jan-02, Extended functionality to add tracks
+
+if __name__ == '__main__':
+    raise Exception('This file is not meant to run by itself')
+
+import DataClasses as DC
+# import ProcessingClasses as PC never used?
+
+
+class FileIO:
+    """Processes data to and from file:
+
+    properties:
+
+    methods:
+        save_inventory(file_name, lst_Inventory): -> None
+        load_inventory(file_name): -> (a list of CD objects)
+
+    """
+    @staticmethod
+    def save_inventory(file_name: list, lst_Inventory: list) -> None:
+        """
+
+
+        Args:
+            file_name (list): list of file names [CD Inventory, Track Inventory] that hold the data.
+            lst_Inventory (list): list of CD objects.
+
+        Returns:
+            None.
+
+        """
+        file_name_CD = file_name[0]
+        file_name_tracks = file_name[1]
+        try:
+            with open(file_name_CD, 'w') as cfile:
+                for disc in lst_Inventory:
+                    cfile.write(disc.get_record())
+            with open(file_name_tracks, 'w') as tFile:
+                tracks_temp_total = ''
+                for index, disc in enumerate(lst_Inventory):
+                    for track in disc.cd_tracks:  # CDs must have tracks?
+                        tracks_temp_total += str(index) + ',' + track.get_record()  # Adding a disc index here!
+                tFile.write(tracks_temp_total)
+        except Exception as e:
+            print('There was a general error!', e, e.__doc__, type(e), sep='\n')
+
+    @staticmethod
+    def load_inventory(file_name: list) -> list:
+        """
+
+
+        Args:
+            file_name (list): list of file names [CD Inventory, Track Inventory] that hold the data.
+
+        Returns:
+            list: list of CD objects.
+
+        """
+        print("Loading")
+        lst_Inventory = []
+        file_name_CD = file_name[0]
+        file_name_tracks = file_name[1]
+        try:
+            with open(file_name_CD, 'r') as file:
+                for line in file:
+                    data = line.strip().split(',')
+                    row = DC.CD(int(data[0]), data[1], data[2])
+                    lst_Inventory.append(row)
+            with open(file_name_tracks, 'r') as file:
+                for line in file:
+                    data = line.strip().split(',')
+                    row = DC.Track(int(data[1]), data[2], data[3])
+                    lst_Inventory[int(data[0])].add_track(row)
+        except Exception as e:
+            print('There was a general error!', e, e.__doc__, type(e), sep='\n')
+        return lst_Inventory
+
+
+class ScreenIO:
+    """Handling Input / Output"""
+
+    @staticmethod
+    def print_menu():
+        """Displays a menu of choices to the user
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+
+        print('Main Menu\n\n[l] load Inventory from file\n[a] Add CD / Album\n[d] Display Current Inventory')
+        print('[c] Choose CD / Album\n[s] Save Inventory to file\n[x] exit\n')
+
+    @staticmethod
+    def menu_choice():
+        """Gets user input for menu selection
+
+        Args:
+            None.
+
+        Returns:
+            choice (string): a lower case sting of the users input out of the choices l, a, d, c, s or x
+
+        """
+        choice = ' '
+        while choice not in ['l', 'a', 'd', 'c', 's', 'x']:
+            choice = input('Which operation would you like to perform? [l, a, d, c, s or x]: ').lower().strip()
+        print()  # Add extra space for layout
+        return choice
+
+    @staticmethod
+    def print_CD_menu():
+        """Displays a sub menu of choices for CD / Album to the user
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+
+        print('CD Sub Menu\n\n[a] Add track\n[d] Display cd / Album details\n[r] Remove track\n[x] exit to Main Menu')
+
+    @staticmethod
+    def menu_CD_choice():
+        """Gets user input for CD sub menu selection
+
+        Args:
+            None.
+
+        Returns:
+            choice (string): a lower case sting of the users input out of the choices a, d, r or x
+
+        """
+        choice = ' '
+        while choice not in ['a', 'd', 'r', 'x']:
+            choice = input('Which operation would you like to perform? [a, d, r or x]: ').lower().strip()
+        print()  # Add extra space for layout
+        return choice
+
+    @staticmethod
+    def show_inventory(table):
+        """Displays current inventory table
+
+
+        Args:
+            table (list of dict): 2D data structure (list of dicts) that holds the data during runtime.
+
+        Returns:
+            None.
+
+        """
+        print('======= The Current Inventory: =======')
+        print('ID\tCD Title (by: Artist)\n')
+        for row in table:
+            print(row)
+        print('======================================')
+
+    @staticmethod
+    def show_tracks(cd):
+        """Displays the Tracks on a CD / Album
+
+        Args:
+            cd (CD): CD object.
+
+        Returns:
+            None.
+
+        """
+        print('====== Current CD / Album: ======')
+        print(cd)
+        print('=================================')
+        print(cd.get_tracks())
+        print('=================================')
+
+    @staticmethod
+    def get_CD_info():
+        """function to request CD information from User to add CD to inventory
+
+
+        Returns:
+            cdId (string): Holds the ID of the CD dataset.
+            cdTitle (string): Holds the title of the CD.
+            cdArtist (string): Holds the artist of the CD.
+
+        """
+
+        cdId = input('Enter ID: ').strip()
+        cdTitle = input('What is the CD\'s title? ').strip()
+        cdArtist = input('What is the Artist\'s name? ').strip()
+        return cdId, cdTitle, cdArtist
+
+    @staticmethod
+    def get_track_info():
+        """function to request Track information from User to add Track to CD / Album
+
+
+        Returns:
+            trkId (string): Holds the ID of the Track dataset.
+            trkTitle (string): Holds the title of the Track.
+            trkLength (string): Holds the length (time) of the Track.
+
+        """
+
+        trkId = input('Enter Position on CD / Album: ').strip()
+        trkTitle = input('What is the Track\'s title? ').strip()
+        trkLength = input('What is the Track\'s length? ').strip()
+        return trkId, trkTitle, trkLength
